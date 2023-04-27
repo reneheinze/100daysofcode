038e7ced72eda928b9fe69d092b040b04bfcedd3
rene.heinze@mailbox.org <(Xdktp6T&7k8A@yt8rUs@fdqrk>
2023-04-27 18:40:44 +0200

    Laptop Migration -> Update Project Configurtion

--- day19/179/main.py
+++ day19/179/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 
+
 def create_turtle_objects(color_list):
     """Get a string color list as input and create and return a turtle object list"""
     turtles = []
@@ -9,31 +10,32 @@ def create_turtle_objects(color_list):
         turtles.append(turtle)
     return turtles
 
+
 def set_start_position(turtle_objects):
     """Get a turtle object list and set them in position"""
-    #get the total number of turtle objects
+    # get the total number of turtle objects
     total_turtles = len(turtle_object)
-    #get the start distance between the turtles
+    # get the start distance between the turtles
     start_distance = (screen_height / 2) / total_turtles
-    #get the start position
-    start_position = (start_distance*total_turtles)/2
-    #iterate through the turtle objects and bring them in position
+    # get the start position
+    start_position = (start_distance * total_turtles) / 2
+    # iterate through the turtle objects and bring them in position
     for turtle in turtle_objects:
         turtle.penup()
-        turtle.goto(x=-450,y=start_position)
+        turtle.goto(x=-450, y=start_position)
         start_position -= start_distance
 
 
 screen = Screen()
-screen_width=1000
-screen_height=1000
+screen_width = 1000
+screen_height = 1000
 
 # Screen setup with a windows of 1000x1000 Pixel
 # Using positional arguments to better understand whats going on later
-screen.setup(width=screen_width,height=screen_height)
+screen.setup(width=screen_width, height=screen_height)
 
 # Messagebox to ask for the winner
-#user_bet = screen.textinput(title="Make you bet", prompt="Which turtle will win the race? Enter a color: ")
+# user_bet = screen.textinput(title="Make you bet", prompt="Which turtle will win the race? Enter a color: ")
 color_list = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 # Create turtle objects
@@ -43,11 +45,10 @@ set_start_position(turtle_object)
 
 print(turtle_object[0].position())
 
-
 # Start Listening for Events
 screen.listen()
-#screen.onkey(move_forwards,"w")
+# screen.onkey(move_forwards,"w")
 
 screen.exitonclick()
 
-#test
+# test
